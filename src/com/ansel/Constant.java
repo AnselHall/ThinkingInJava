2b2b43b30b9471dc6129d8628f8c59fb6a9cbd69
AnselHall <1206313836@qq.com>
2016-05-16 17:07:06 +0800

    1

---
+++ src/com/ansel/Constant.java
@@ -0,0 +1,8 @@
+package com.ansel;
+
+/**
+ * Created by user on 2016/5/16.
+ */
+public class Constant {
+    public static final String FILEPATH = "E:/Personal/IDEA/src";
+}
--- src/io/BufferedInputFile.java
+++ src/io/BufferedInputFile.java
@@ -1,22 +1,32 @@
 package io;//: io/BufferedInputFile.java
-import java.io.*;
+
+import com.ansel.Constant;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class BufferedInputFile {
-  // Throw exceptions to console:
-  public static String
-  read(String filename) throws IOException {
-    // Reading input by lines:
-    BufferedReader in = new BufferedReader(
-      new FileReader(filename));
-    String s;
-    StringBuilder sb = new StringBuilder();
-    while((s = in.readLine())!= null)
-      sb.append(s + "\n");
-    in.close();
-    return sb.toString();
-  }
-  public static void main(String[] args)
-  throws IOException {
-    System.out.print(read("BufferedInputFile.java"));
-  }
-} /* (Execute to see output) *///:~
+    // Throw exceptions to console:
+    public static String
+    /**
+     * 接收一个文件名称，打印文件中的内容
+     */
+    read(String filename) throws IOException {
+        // Reading input by lines:
+        BufferedReader in = new BufferedReader(
+                new FileReader(filename));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while ((s = in.readLine()) != null)
+            //sb.append(s + "\n"); 与sb.append(s).append("\n");有什么区别？
+            sb.append(s+"\n");
+        in.close();
+        return sb.toString();
+    }
+
+    public static void main(String[] args)
+            throws IOException {
+        System.out.print(read(Constant.FILEPATH + "/io/BufferedInputFile.java"));
+    }
+}
